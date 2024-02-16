39c21d3641c760898f4f9e3d8655b28a5040a4b8
Pratay Nandy <prataynandy147@gmail.com>
2024-02-16 12:51:49 +0530

    frontendchanged

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://snappy-ui.netlify.app",
     credentials: true,
   },
 });
